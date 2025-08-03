d0b78542436cdd1bf4c3cb50cff38b093cfe78ce
문은서 <meunse1999@naver.com>
2025-08-03 22:34:48 +0900

    chore: .gitignore 수정

--- Documents/dev/sprint_mission_6/src/main/java/com/sprint/mission/discodeit/mapper/PageResponseMapper.java
+++ Documents/dev/sprint_mission_6/src/main/java/com/sprint/mission/discodeit/mapper/PageResponseMapper.java
@@ -1,4 +1,27 @@
 package com.sprint.mission.discodeit.mapper;
 
+import com.sprint.mission.discodeit.response.PageResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
+
 public class PageResponseMapper {
+    public <T> PageResponse<T> fromSlice(Slice<T> slice) {
+        return new PageResponse<>(
+                slice.getContent(),
+                slice.getNumber(),
+                slice.getSize(),
+                slice.hasNext(),
+                null
+        );
+    }
+
+    public <T> PageResponse<T> fromPage(Page<T> page) {
+        return new PageResponse<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.hasNext(),
+                null
+        );
+    }
 }
