e5ed295cd2f06ec3a7e29618e0ee9ab09d968279
문은서 <meunse1999@naver.com>
2025-10-28 13:32:19 +0900

    이전 과제 파일 완전 제거

---
+++ build/generated/sources/annotationProcessor/java/main/com/sprint/mission/discodeit/mapper/PageResponseMapperImpl.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-10-27T13:36:28+0900",
+    comments = "version: 1.6.3, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.10.2.jar, environment: Java 17.0.15 (Microsoft)"
+)
+@Component
+public class PageResponseMapperImpl implements PageResponseMapper {
+}
